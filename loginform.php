e88ee18a7bb01183bb807db5c4664777516f9f1f
hiro1957 <136159793+hiro1957@users.noreply.github.com>
2023-07-26 19:44:24 +0900

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -32,15 +32,15 @@
        
             <label>メールアドレス</label></br>
            
-            <input type="text" id="inputmail"  name="email" required name="required">
+            <input type="text" id="inputmail"  name="email" >
         </p>
 
             <label>パスワード</label></br>
-            <input type="password" id="inputpassword" name="password" required name="required" maxlength="10"></br>
+            <input type="password" id="inputpassword" name="password"  maxlength="10"></br>
             (※10文字以内で入力してください)
         </p>
 
-        <button type="submit"name="submit" class="btn btn-primary">ログイン</button>
+        <button type="submit"name="submit" id="btnSubmit" class="btn btn-primary">ログイン</button>
     </form>
 
 <p><a href='form.php'>登録をしていない方はこちらから</a></p>
@@ -49,4 +49,34 @@
 </body>
 </html>
 
+
+<script>
+    window.onload = function(){
+        /*各画面オブジェクト*/
+        const btnSubmit = document.getElementById('btnSubmit');
+        const inputMail = document.getElementById('inputmail');
+        const inputPassword = document.getElementById('inputpassword');
+    
+        const reg = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/;
+        
+        
+        btnSubmit.addEventListener('click', function(event) {
+            let message = [];
+            /*入力値チェック*/
+            if(inputPassword.value ==""){
+                message.push("氏名が未入力です。");
+            }
+            if(inputMail.value==""){
+                message.push("メールアドレスが未入力です。");
+            }else if(!reg.test(inputMail.value)){
+                message.push("メールアドレスの形式が不正です。");
+            }
+            if(message.length > 0){
+                alert(message);
+                return;
+            }
+            alert('入力チェックOK');
+        });
+    };
+</script>
 
